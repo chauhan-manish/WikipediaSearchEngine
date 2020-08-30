2e4f2ac0afb0c0bc982561b4371be2fa552371b3
Manish Chauhan <chauhanmanish1258@gmail.com>
2020-08-30 14:15:20 +0530

    Add files via upload

--- src/Phase1/indexer.py
+++ src/Phase1/indexer.py
@@ -1,21 +1,27 @@
-import re, os, sys
 import xml.sax
-from xml.sax import parse, ContentHandler
-import timeit
-import xml.sax
+import re, os, sys, timeit
 from collections import defaultdict
+from xml.sax import parse, ContentHandler
+from nltk.stem.snowball import SnowballStemmer
 
 stop_words = set()
+words_in_index = set()
 inverted_index = defaultdict(lambda:defaultdict(lambda:defaultdict(int)))
 
 index_path = ""
 invertedindex_stat = ""
 
-doc_id_to_data = None
-push_limit = 4000
+push_limit = 5000
 last_doc_id = 0
+file_no = 0
 
-#RegEx to remove URLs
+tokens_in_dump = 0
+tokens_in_index = 0
+
+#Stemmer object
+stemmer = SnowballStemmer("english")
+
+#RegEx to remove URLs, CSS, Punctuations
 regExp1 = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.DOTALL)
 #RegEx to remove CSS
 regExp2 = re.compile(r'{\|(.*?)\|}', re.DOTALL)
@@ -27,13 +33,10 @@ regExp4 = re.compile(r'[-.,:;_?()"/\']', re.DOTALL)
 regExp5 = re.compile(r'\[\[file:(.*?)\]\]', re.DOTALL)
 #RegEx to remove Special Characters
 regExp6 = re.compile(r'[~`!@#$%-^*+{\[}\]\|\\<>/?]', re.DOTALL)
-#RegEx to remove {{.*}} from text
-regExp7 = re.compile(r'{{(.*?)}}', re.DOTALL)
 #RegEx to remove <..> from text
-regExp8 = re.compile(r'<(.*?)>' ,re.DOTALL)
+regExp7 = re.compile(r'<(.*?)>' ,re.DOTALL)
 
-
-# Function t0 clean the text
+# Function to clean the text
 def cleanText(text):
 	try:
 		text = regExp1.sub('', text)
@@ -41,30 +44,18 @@ def cleanText(text):
 		text = regExp3.sub('', text)
 		text = regExp4.sub('', text)
 		text = regExp5.sub('', text)
-		text = regExp8.sub('', text)
+		text = regExp7.sub('', text)
 	except:
 		return text
 	return text
 
-def docMappingDump(doc_id, doc_title, doc_data):
-	doc_id = str(doc_id)
-	doc_title = str(doc_title)
-	#print(doc_id, doc_title)
-	try:
-		if doc_id_to_data is not None:
-			doc_id_to_data.write(doc_id + "#" + doc_title + "\n")
-		else:
-			print("Found None")
-	except:
-		print("Some Error Occured")
-
-def dumpInvertedIndex(doc_id):
+def dumpInvertedIndex(doc_id, file_no):
 	global index_path, inverted_index
 	if any(inverted_index) == False:
 		print("No Entries to Dump")
 
 	#print(inverted_index)
-	fout = open(index_path + str(doc_id) + ".txt", "w")
+	fout = open(index_path + str(file_no) + ".txt", "w")
 	for key,val in  sorted(inverted_index.items()):
 		try:
 			if key == None or val == None:
@@ -97,15 +88,22 @@ def dumpInvertedIndex(doc_id):
 
 	
 def insertIntoInvertedIndex(final_words, doc_id, ch):
-	global inverted_index
+	global inverted_index, tokens_in_dump, tokens_in_index
 	for word in final_words:
 		try:
 			word = word.strip()
 			if word.isalpha() and len(word) > 3 and word not in stop_words:
+				tokens_in_dump += 1
+
+				word = stemmer.stem(word)
 				if word not in stop_words:
-					if word in inverted_index:
-						if doc_id in  inverted_index[word]:
-							if ch in inverted_index[word][doc_id]:
+					if word not in words_in_index:
+						tokens_in_index += 1
+						words_in_index.add(word)
+
+					if word in inverted_index.keys():
+						if doc_id in  inverted_index[word].keys():
+							if ch in inverted_index[word][doc_id].keys():
 								inverted_index[word][doc_id][ch] += 1
 							else:
 								inverted_index[word][doc_id][ch] = 1
@@ -146,67 +144,69 @@ def cleanExtractedData(extracted_dict, doc_id):
 	if len(extracted_dict["Links"]):
 		cleanExtractedDict(extracted_dict, doc_id, "Links", "e")
 	
-
-def processBuffer(doc_data, doc_id, is_title, is_text):
+def processText(doc_data, doc_id):
 	text = doc_data.lower()
 	text = cleanText(text)
 
-	if is_title:
-		words = text.split()
-		final_words = []
-		for word in words:
-			try:
-				if word.isalpha() and word not in stop_words:
-					final_words.append(regExp6.sub(' ', word))
-			except:
-				continue
-		insertIntoInvertedIndex(final_words, doc_id, "t")
-
-	elif is_text:
-		extracted_dict = {}
-		extracted_dict["Body"] = []
-		extracted_dict["InfoBox"] = []
-		extracted_dict["Categories"] = []
-		extracted_dict["References"] = []
-		extracted_dict["Links"] = []
-		
-		lines = text.split("\n")
-		index = 0
-		length = len(lines)
-		while index < length:
-			try:
-				if lines[index].startswith("{{infobox"):
-					while True:
-						if (index + 1) >= length or lines[index + 1].endswith("}}"):
+	extracted_dict = {}
+	extracted_dict["Body"] = []
+	extracted_dict["InfoBox"] = []
+	extracted_dict["Categories"] = []
+	extracted_dict["References"] = []
+	extracted_dict["Links"] = []
+	
+	lines = text.split("\n")
+	index = 0
+	length = len(lines)
+	while index < length:
+		try:
+			if lines[index].startswith("{{infobox"):
+				while True:
+					if (index + 1) >= length or lines[index + 1].endswith("}}"):
+						break
+					index += 1
+					extracted_dict["InfoBox"].append(lines[index])
+			elif "[[category" in lines[index]:
+				line = lines[index][11 : -2]
+				extracted_dict["Categories"].append(line)
+			elif lines[index].startswith("="):
+				title_text = lines[index].replace("=", "")
+				if title_text == "references" or title_text == "see also" or title_text == "further reading" or title_text == "external links":
+					while index < length:
+						if (index + 1) >= length or lines[index + 1].startswith("="):
 							break
 						index += 1
-						extracted_dict["InfoBox"].append(lines[index])
-				elif "[[category" in lines[index]:
-					line = lines[index][11 : -2]
-					extracted_dict["Categories"].append(line)
-				elif lines[index].startswith("="):
-					title_text = lines[index].replace("=", "")
-					if title_text == "references" or title_text == "see also" or title_text == "further reading" or title_text == "external links":
-						while index < length:
-							if (index + 1) >= length or lines[index + 1].startswith("="):
-								break
-							index += 1
-							if title_text == "references":
-								extracted_dict["References"].append(lines[index])
-							elif title_text == "external links" and lines[index].startswith("*"):
-								extracted_dict["Links"].append(lines[index])
-				else:
-					extracted_dict["Body"].append(lines[index])
+						if title_text == "references":
+							extracted_dict["References"].append(lines[index])
+						elif title_text == "external links" and lines[index].startswith("*"):
+							extracted_dict["Links"].append(lines[index])
+			else:
+				extracted_dict["Body"].append(lines[index])
 
-			except:
-				continue
+		except:
 			index += 1
+			continue
+		index += 1
 
-		cleanExtractedData(extracted_dict, doc_id)
-		global last_doc_id
-		last_doc_id = doc_id
-		if doc_id % push_limit == 0:
-			dumpInvertedIndex(doc_id)
+	cleanExtractedData(extracted_dict, doc_id)
+	global last_doc_id, file_no
+	last_doc_id = doc_id
+	if doc_id % push_limit == 0:
+		dumpInvertedIndex(doc_id, file_no)
+		file_no += 1
+
+def processTitle(doc_data, doc_id):
+	text = doc_data.lower()
+	text = cleanText(text)
+	words = text.split()
+	final_words = []
+	for word in words:
+		try:
+			if word.isalpha() and word not in stop_words:
+				final_words.append(regExp6.sub(' ', word))
+		except:
+			continue
+	insertIntoInvertedIndex(final_words, doc_id, "t")
 
 class WikiHandler(xml.sax.handler.ContentHandler):
 	def __init__(self):
@@ -238,20 +238,15 @@ class WikiHandler(xml.sax.handler.ContentHandler):
 
 	def endElement(self, tag):
 		if tag == "title":
-			processBuffer(self.doc_data, self.doc_id, True, False)
+			processTitle(self.doc_data, self.doc_id)
 			self.is_title = True
 			self.doc_title = self.doc_data
 			self.doc_data = ""
 
 		elif tag == "text":
-			processBuffer(self.doc_data, self.doc_id, False, True)
+			processText(self.doc_data, self.doc_id)
 			self.is_text = True
 			self.buffer = ""
-
-		elif tag == "id" and self.is_firstID:
-			#docMappingDump(self.doc_id, self.doc_title, self.doc_data)
-			self.is_firstID = False
-			self.doc_data = ""
 
 	def characters(self, content):
 		self.doc_data = self.doc_data + content
@@ -262,17 +257,20 @@ def parseXML(dump_path):
 	parser.setContentHandler(handler)
 	parser.parse(dump_path)
 
+def loadStopWords():
+	global stop_words
+	fin = open("./2019201052/stopWords.txt", "r")
+	for line in fin:
+		line = line.strip()
+		stop_words.add(line)
+	fin.close()
+
 def main():
 	if len(sys.argv) != 4:
 		print("Usage: bash index.sh wiki_dump.xml output_path invertedindex_stat.txt")
 		sys.exit(0);
 
-	global index_path, stop_words, doc_id_to_data, invertedindex_stat
-	
-	fin = open("stopWords.txt", "r")
-	for line in fin:
-		line = line.strip()
-		stop_words.add(line)
+	global index_path, doc_id_to_data, invertedindex_stat
 	
 	dump_path = sys.argv[1]
 	index_path = sys.argv[2]
@@ -284,11 +282,14 @@ def main():
 	if index_path[-1:] != '/':
 		index_path += '/'
 
-	doc_id_to_data = open(index_path + "docTitleMap.txt", "w")
-	
 	print("Parsing Started...")
 	parseXML(dump_path)
-	dumpInvertedIndex(last_doc_id)
+	dumpInvertedIndex(last_doc_id, file_no)
+
+	fout = open(invertedindex_stat, "w")
+	fout.write(str(tokens_in_dump) + "\n")
+	fout.write(str(tokens_in_index) + "\n")
+	fout.close()
 
 
 if __name__ == "__main__":
